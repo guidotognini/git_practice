155ebdb7607bc728123119c12981523d88efaba9
Guido Tognini <guidotognini@gmail.com>
2023-04-27 21:43:58 -0300

    Creates functions to pick words in the arrays

--- mixed_messages.js
+++ mixed_messages.js
@@ -2,9 +2,9 @@ const subject = ['swimmer', 'boxer', 'gymnast', 'cyclist'];
 const action = ['bounces', 'shots', 'hits'];
 const object = ['basketball', 'soccer ball', 'volleyball', 'golf ball'];
 
-let sub_element = subject[Math.floor(Math.random()*subject.length)]
-let act_element = action[Math.floor(Math.random()*action.length)]
-let obj_element = object[Math.floor(Math.random()*object.length)]
+const word = arr => {
+  return arr[Math.floor(Math.random()*arr.length)]
+}
 
-console.log(`The ${sub_element} has grown tired of his former sport and now ${act_element} the ${obj_element} to try something new`)
+console.log(`The ${word(subject)} has grown tired of his former sport and now ${word(action)} the ${word(object)} to try something new`)
 
