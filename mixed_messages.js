3fc1f2ecc8f5e896899e6398c74bcd187c8bed79
Guido Tognini <guidotognini@gmail.com>
2023-04-27 21:47:43 -0300

    Add comments to the code

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,10 +1,13 @@
+//Declare three arrays containing the words
 const subject = ['swimmer', 'boxer', 'gymnast', 'cyclist'];
 const action = ['bounces', 'shots', 'hits'];
 const object = ['basketball', 'soccer ball', 'volleyball', 'golf ball'];
 
+//Declares the function to randomly pick a word in the array
 const word = arr => {
-  return arr[Math.floor(Math.random()*arr.length)]
+  return arr[Math.floor(Math.random()*arr.length)];
 }
 
-console.log(`The ${word(subject)} has grown tired of his former sport and now ${word(action)} the ${word(object)} to try something new`)
+//Prints a phrase to the user with the 3 random words as arguments
+console.log(`The ${word(subject)} has grown tired of his former sport and now ${word(action)} the ${word(object)} to try something new`);
 
